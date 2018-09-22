bc0be4a845df54a3732a9001adfe278b15399bb9
Jeremiasz Pelczar <jeremiasz.pelczar@gmail.com>
2018-09-22 15:30:34 +0200

    Vertical alignment

--- luma_tetris.py
+++ luma_tetris.py
@@ -7,8 +7,6 @@ import random
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
-from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 from luma.core.render import canvas
 
 class block:
@@ -22,7 +20,7 @@ class block:
 
     def draw(self):
         with canvas(self.dev) as draw:
-            draw.point(self.xy, fill="white")    
+            draw.point((15 - self.xy[1], self.xy[0]), fill="white")    
     
 def create_device():
     serial = spi(port=0, device=0, gpio=noop())
@@ -36,7 +34,12 @@ if __name__ == "__main__":
     dev = create_device()
 
     b = block(dev)
-    b.move((7,7))
+    b.move((0,0))
+    b.draw()
+
+    time.sleep(1)
+
+    b.move((7,0))
     b.draw()
 
     time.sleep(1)
