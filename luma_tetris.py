213db6b8e0c3c4dfdaeb19e389e2735c43d7cf93
Paweł Pelczar <paweusz@gmail.com>
2018-10-10 21:39:23 +0200

    Move block handling to scene

--- luma_tetris.py
+++ luma_tetris.py
@@ -15,27 +15,18 @@ class Game:
         self.blocks = [OBlock, LBlock, IBlock, ZBlock, SBlock, JBlock, TBlock]
 
         self.scene = Scene(dev, (8,16))
-        self.block = self.random_block()
-        self.scene.set_current_block(self.block)
+        self.scene.set_block(self.random_block())
         
     def tick(self):
-        if (not self.move_block((0,1))):
-            self.scene.hydrate_block(self.block)
+        if (not self.scene.move_block((0,1))):
+            self.scene.hydrate_block()
             self.scene.process_lines()
-            self.block = self.random_block()
-            self.scene.set_current_block(self.block)
+            self.scene.set_block(self.random_block())
 
         self.scene.render()
 
         self.timer = Timer(1, self.tick)
         self.timer.start()
-
-    def move_block(self, xy):
-        self.block.move(xy)
-        if (self.scene.collides(self.block)):
-            self.block.move((-xy[0], -xy[1]))
-            return False
-        return True
 
     def random_block(self):
         block_idx = random.randint(0,len(self.blocks)-1)
@@ -48,21 +39,19 @@ class Game:
             ch = self.getch()
 
             if (ch == ' '):
-                self.block.rotation = self.block.rotation - 1
-                if (self.block.rotation == -1):
-                    self.block.rotation = 3
+                self.scene.rotate_block()
 
             if (ch == 's'):
-                self.move_block((0,1))
+                self.scene.move_block((0,1))
 
             if (ch == 'w'):
-                self.move_block((0,-1))
+                self.scene.move_block((0,-1))
 
             if (ch == 'd'):
-                self.move_block((1,0))
+                self.scene.move_block((1,0))
 
             if (ch == 'a'):
-                self.move_block((-1,0))
+                self.scene.move_block((-1,0))
 
             if (ch == 'q'):
                 self.timer.cancel()
--- scene.py
+++ scene.py
@@ -6,8 +6,20 @@ class Scene:
         self.size = size
         self.bg = [[False for i in range(size[0])] for i in range(size[1])]
 
-    def set_current_block(self, block):
+    def set_block(self, block):
         self.block = block
+        
+    def move_block(self, xy):
+        self.block.move(xy)
+        if (self.collides()):
+            self.block.move((-xy[0], -xy[1]))
+            return False
+        return True
+
+    def rotate_block(self):
+        self.block.rotation = self.block.rotation - 1
+        if (self.block.rotation == -1):
+            self.block.rotation = 3
 
     def render(self):
         self.dev.clean()
@@ -20,14 +32,14 @@ class Scene:
 
         self.dev.render()
         
-    def hydrate_block(self, block):
+    def hydrate_block(self):
         for my_i in range(4):
-            my_xy = block.get_xy(my_i)
+            my_xy = self.block.get_xy(my_i)
             self.bg[my_xy[1]][my_xy[0]] = 1
 
-    def collides(self, block):
+    def collides(self):
         for my_i in range(4):
-            my_xy = block.get_xy(my_i)
+            my_xy = self.block.get_xy(my_i)
 
             if (my_xy[0] == self.size[0] or my_xy[0] == -1 or my_xy[1] == self.size[1] or my_xy[1] == -1):
                 return True
@@ -39,11 +51,11 @@ class Scene:
 
     def process_lines(self):
         for y in range(self.size[1]):
-            if self.full_line(y):
+            if self.is_full_line(y):
                 del self.bg[y]
                 self.bg.insert(0, [False for i in range(self.size[0])])
     
-    def full_line(self, y):
+    def is_full_line(self, y):
         for x in range(self.size[0]):
             if not self.bg[y][x]:
                 return False
